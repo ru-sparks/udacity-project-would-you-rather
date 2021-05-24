e587393e3cc45e9cf50ba71dda7e9342e5571ae6
James Sparks <ru_sparks@yahoo.com>
2021-05-24 16:24:48 -0700

    radio button work

--- src/protected/QuestionAsk.js
+++ src/protected/QuestionAsk.js
@@ -1,29 +1,63 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../App.css";
+import {
+  Form,
+  Button,
+} from "react-bootstrap";
 
-const QuestionAsk = (props) => {
-    const { questionId } = useParams();
+class QuestionAsk extends Component {
+  state = {
+    selected: "0",
+  };
+  formSubmit = (event) => {
+    alert(this.state.selected);
+    event.preventDefault();
+  };
+  onValueChange = (event) => {
+    this.setState({
+      selected: event.target.value,
+    });
+  };
+  render() {
+    let { questionId } = this.props.match.params;
 
-    let question = props.items.questions[questionId];
+    let question = this.props.items.questions[questionId];
+
     if (question) {
-        return (
-            <div>
-                <h1>Question Ask</h1>
-                <p>{question.optionOne.text}</p>
-                <p>{question.optionTwo.text}</p>
-            </div>
-        );
-    
+      return (
+        <>
+          <h3>Would you Rather?</h3>
+          <Form onSubmit={this.formSubmit}>
+            <Form.Check
+              type="radio"
+              value="1"
+              checked={this.state.selected === "1"}
+              onChange={this.onValueChange}
+              label={question.optionOne.text}
+            />
+            <Form.Check
+              type="radio"
+              value="2"
+              checked={this.state.selected === "2"}
+              onChange={this.onValueChange}
+              label={question.optionOne.text}
+            />
+            <Button variant="outline-secondary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </>
+      );
     } else {
-        return (<div>404 Not Found</div>);
+      return <div>404 Not Found</div>;
     }
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      items: state.items,
-    };
+  return {
+    items: state.items,
   };
+};
 export default connect(mapStateToProps)(QuestionAsk);
- 
